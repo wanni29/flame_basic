d3d4e8494bb8a14974760fcf21f49b364269ab80
wanni30 <hubhy@nate.com>
2025-02-08 09:29:08 +0900

    사과 게임 만들기

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,17 @@
-import 'package:flame/game.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+import 'package:flame_basic/basic/apple_world.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
-  final myGame = FlameGame(world: MyWorld());
-  runApp(
-    GameWidget(game: myGame),
+  final myGame = FlameGame(
+    world: AppleWorld(),
+    camera: CameraComponent.withFixedResolution(
+      width: 800,
+      height: 600,
+    )..viewfinder.anchor = Anchor.topLeft,
   );
-}
-
-class MyWorld extends World {
-  @override
-  Future<void> onLoad() async {
-    await add(MyCreate());
-  }
-}
-
-class MyCreate extends SpriteComponent {
-  // A component that renders the create sprite, with a 16 x 16 size.
-  MyCreate() : super(size: Vector2.all(16));
-
-  @override
-  Future<void> onLoad() async {
-    sprite = await Sprite.load('player.png');
-  }
+  runApp(
+    GameWidget(game: myGame), // Flutter 에서 실행
+  );
 }
